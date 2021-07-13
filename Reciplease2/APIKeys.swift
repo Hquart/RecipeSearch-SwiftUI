b55023a814edaa13fb107eeaf9e0324dfe07e782
Hquart <hquart@icloud.com>
2021-07-13 18:20:54 +0200

    modified APIKEys

---
+++ Reciplease2/APIKeys.swift
@@ -0,0 +1,14 @@
+//
+//  APIKeys.swift
+//  Reciplease2
+//
+//  Created by Naji Achkar on 25/04/2021.
+//
+
+import Foundation
+
+struct APIKeys {
+    
+    static let app_id: String = "xxxxxxx"
+    static let app_key: String = "xxxxxxxxxxxxxxxxxxxxxxxx"
+}
--- Reciplease2/Extensions/View.swift
+++ Reciplease2/Extensions/View.swift
@@ -26,29 +26,6 @@ extension URLComponents {
     }
 }
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//extension Bundle {
-//    func decode<T: Codable>(_ file: String) -> T {
-//        guard let url = self.url(forResource: file, withExtension: nil) else {
-//            fatalError("Failed to locate \(file) in bundle.")
-//        }
-//
-//        guard let data = try? Data(contentsOf: url) else {
-//            fatalError("Failed to load \(file) from bundle.")
-//        }
-//
-//        let decoder = JSONDecoder()
-//
-//        let formatter = DateFormatter()
-//        formatter.dateFormat = "y-MM-dd"
-//        decoder.dateDecodingStrategy = .formatted(formatter)
-//
-//        guard let loaded = try? decoder.decode(T.self, from: data) else {
-//            fatalError("Failed to decode \(file) from bundle.")
-//        }
-//        return loaded
-//    }
-//}
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Additionnal custom colors:
 extension Color {
     static let appGreen = Color("appGreen")
